eb4f60addf4a5781fbe4b46efaa0f31f86fccd0d
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-09-05 11:45:03 -0400

    button and coins created

---
+++ coinFlip.js
@@ -0,0 +1,35 @@
+
+class CoinFlip extends React.Component{
+    //this is a child or subclass; it's a child of React.component
+    //react.component is the parent or super class
+    //we get all the cool stuff because it's a subclass
+    constructor(){
+        super();
+        //react doesn't care about this at all. This is regular JS.
+        this.heads = "buffalo-nickel-heads.jpg"
+        this.tails = "buffalo-nickel-tails.png"
+        this.coin = [
+            this.heads,
+            this.tails
+        ]
+        //react cares about state *a lot*. React is special
+        this.state= {
+            image: this.coin[0]
+        }
+        }
+    render(){
+        //it is render's job to return a single DOM element!!
+        //whether you like it or not, render will run right after the constructor;
+        return(
+            <img src={this.state.image} />
+        )
+    }
+}
+
+
+
+//reactDOM.render takes 2 things: WHAT and WHERE
+ReactDOM.render(
+    <CoinFlip />,
+    document.querySelector('#root')
+)
